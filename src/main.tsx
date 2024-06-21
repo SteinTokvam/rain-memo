7561a422b7b2851c68e86656d9d146cb1bc7dcf1
Stein Petter Tokvam <stto1210@gmail.com>
2024-06-21 15:29:27 +0200

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
+        <Analytics />
         <App />
       </Provider>
     </BrowserRouter>
