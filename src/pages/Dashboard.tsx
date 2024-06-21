cbf8e3cdecf4c55cc7cd63eb5d054747518216af
Stein Petter Tokvam <stto1210@gmail.com>
2024-06-21 14:53:34 +0200

    skip dashboard view if only one device

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, Divider, Spinner } from "@nextui-org/react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -38,6 +38,9 @@ export default function Dashboard({ supabase }: { supabase: SupabaseClient }) {
         .then((res) => res.json())
         .then((data) => {
           dispatch(setStations(data));
+          if (data.length === 1) {
+            navigate(routes.device.replace(":id", data[0].home_id));
+          }
         });
     });
   }, []);
@@ -45,42 +48,44 @@ export default function Dashboard({ supabase }: { supabase: SupabaseClient }) {
   return (
     <DefaultLayout supabase={supabase}>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <h1 className="text-3xl text-default-600">Dine målestasjoner</h1>
-        <Divider />
-        {stations &&
-          stations.map((station: any) => (
-            <Card
-              key={station.home_id}
-              isHoverable
-              isPressable
-              className="w-full sm:w-2/5 mx-auto bg-slate-200 dark:bg-slate-700"
-              onPress={() => {
-                navigate(routes.device.replace(":id", station.home_id));
-              }}
-            >
-              <CardHeader className="text-3xl text-default-600">
-                <h1 className="text-3xl text-default-600 text-bold">
-                  {station.station_name} ({station.place.street},{" "}
-                  {station.place.city})
-                </h1>
-              </CardHeader>
-              <CardBody>
-                <p className="text-lg text-default-600 text-semibold">
-                  Enheter:
-                </p>
-                {station.modules.map((module: any) => {
-                  return (
-                    <p
-                      key={module.module_name}
-                      className="text-sm text-default-600"
-                    >
-                      {module.module_name}
-                    </p>
-                  );
-                })}
-              </CardBody>
-            </Card>
-          ))}
+        {stations ?
+          <>
+            <h1 className="text-3xl text-default-600">Dine målestasjoner</h1>
+            <Divider />
+
+            {stations.map((station: any) => (
+              <Card
+                key={station.home_id}
+                isHoverable
+                isPressable
+                className="w-full sm:w-2/5 mx-auto bg-slate-200 dark:bg-slate-700"
+                onPress={() => {
+                  navigate(routes.device.replace(":id", station.home_id));
+                }}
+              >
+                <CardHeader className="text-3xl text-default-600">
+                  <h1 className="text-3xl text-default-600 text-bold">
+                    {station.station_name} ({station.place.street},{" "}
+                    {station.place.city})
+                  </h1>
+                </CardHeader>
+                <CardBody>
+                  <p className="text-lg text-default-600 text-semibold">
+                    Enheter:
+                  </p>
+                  {station.modules.map((module: any) => {
+                    return (
+                      <p
+                        key={module.module_name}
+                        className="text-sm text-default-600"
+                      >
+                        {module.module_name}
+                      </p>
+                    );
+                  })}
+                </CardBody>
+              </Card>
+            ))}</> : <Spinner />}
       </section>
     </DefaultLayout>
   );
