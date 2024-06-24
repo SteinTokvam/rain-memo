ad1a5b911e87462e4f12fdbfd0fa0a6015c748a7
Stein Petter Tokvam <stto1210@gmail.com>
2024-06-24 13:44:13 +0200

    call netatmo directly for device

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,6 +7,7 @@ import { SupabaseClient } from "@supabase/supabase-js";
 import { getNetatmoUserData, routes } from "@/global";
 import { setStations } from "@/actions/netatmo";
 import DefaultLayout from "@/layouts/default";
+import { Device, Module } from "@/types";
 
 export default function Dashboard({ supabase }: { supabase: SupabaseClient }) {
   const navigate = useNavigate();
@@ -15,6 +16,7 @@ export default function Dashboard({ supabase }: { supabase: SupabaseClient }) {
   );
 
   const dispatch = useDispatch();
+  const base_url = 'https://api.netatmo.com';
 
   useEffect(() => {
     getNetatmoUserData(supabase).then((res) => {
@@ -26,22 +28,30 @@ export default function Dashboard({ supabase }: { supabase: SupabaseClient }) {
       }
       const access_token = res.data ? res.data.access_token : "";
 
-      fetch("https://rain-memo-backend.onrender.com/netatmo/getStations", {
-        method: "POST",
+      const endpoint = '/api/getstationsdata'
+      fetch(`${base_url}${endpoint}`, {
+        method: 'GET',
         headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          access_token,
-        }),
+          'Authorization': 'Bearer ' + access_token
+        }
       })
-        .then((res) => res.json())
-        .then((data) => {
-          dispatch(setStations(data));
-          if (data.length === 1) {
-            navigate(routes.device.replace(":id", data[0].home_id));
+        .then(response => response.json())
+        .then(data => {
+          const devicesWithRainSensors: Device[] = [];
+          if (data.error) {
+            return
           }
-        });
+          data.body.devices.forEach((device: Device) => {
+            const modules = device.modules.filter((module: Module) => module.type === 'NAModule3')//NAModule3 === rain sensor;
+            modules.length > 0 && devicesWithRainSensors.push(device);
+          });
+
+          dispatch(setStations(devicesWithRainSensors));
+          if (devicesWithRainSensors.length === 1) {
+            navigate(routes.device.replace(":id", devicesWithRainSensors[0].home_id));
+          }
+        })
+        .catch(error => console.error(error));
     });
   }, []);
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,69 @@ export type IconSvgProps = SVGProps<SVGSVGElement> & {
 };
 
 export type NetatmoInfo = { client_id: string; client_secret: string, redirect_uri: string };
+
+export type Device = {
+  _id: string,
+  station_name: string,
+  date_setup: number,
+  last_setup: number,
+  type: string,
+  last_status_store: number,
+  module_name: string,
+  firmware: number,
+  wifi_status: number,
+  reachable: boolean,
+  co2_calibrating: boolean,
+  data_type: string[],
+  place: DeviceLocation,
+  invitation_disable: boolean,
+  home_id: string,
+  home_name: string,
+  read_only: boolean,
+  dashboard_data: DashboardData,
+  modules: Module[]
+}
+
+export type DeviceLocation = {
+  altitude: number,
+  city: string,
+  street: string,
+  country: string,
+  timezone: string,
+  location: number[]
+}
+
+export type DashboardData = { 
+  time_utc: number,
+  Temperature: number,
+  CO2: number,
+  Rain: number,
+  sum_rain_1: number,
+  sum_rain_24: number,
+  Humidity: number,
+  Noise: number,
+  Pressure: number,
+  AbsolutePressure: number,
+  min_temp: number,
+  max_temp: number,
+  date_max_temp: number,
+  date_min_temp: number,
+  temp_trend: string,
+  pressure_trend: string
+}
+
+export type Module = {
+  _id: string,
+  type: string,
+  module_name: string,
+  last_setup: number,
+  data_type: string[],
+  battery_percent: number,
+  reachable: boolean,
+  firmware: number
+  last_message: number,
+  last_seen: number,
+  rf_status: number,
+  battery_vp: number,
+  dashboard_data: DashboardData
+}
